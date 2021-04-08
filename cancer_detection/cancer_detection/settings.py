9f1642b7b977d0e3c260fec36211e6ba9b815fd3
nidhi-30 <60999240+nidhi-30@users.noreply.github.com>
2021-04-08 16:24:11 -0400

    Update settings.py

--- cancer_detection/cancer_detection/settings.py
+++ cancer_detection/cancer_detection/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=2w+ar9uoyi5@siso)rs7yxgsf(28)prse9(wgzl=s-=___1=l'
+SECRET_KEY = '=2w+ar9uoyi5@siso)rs7yxgsf(28)prse9(wgzl=s-=___1=l1212'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
